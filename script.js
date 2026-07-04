1ec227277b23be8936ff1c6f414d26184084ed3c
Olusola Oluwaferanmi <111358938+Ferexy@users.noreply.github.com>
2026-07-04 09:58:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function verify() {
+    let Email = document.getElementById('Mail').innerHTML;
+    if (Email.includes("@")) { console.log("wetin u dey do")
+    }else {
+       let alert= document.getElementById("alert");
+       
+    }
+}
